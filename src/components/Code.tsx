1c53da139392c35c6711585b316f65a5fe9bbc31
whatap-docs <kj.im@whatap.io>
2024-10-15 13:56:28 +0900

    update codes

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -1,29 +1,41 @@
-import { RawCode, Pre, highlight } from "codehike/code"
+import { RawCode, Pre } from "codehike/code"
 import { CopyButton } from "./codes/button"
 import { callout } from "./codes/callout"
 import { className } from "./codes/classname"
+import { diff } from "./codes/diff"
+import { mark } from "./codes/mark"
+import { lineNumbers } from "./codes/line-numbers"
 import clsx from 'clsx'
 import styles from './codes/styles.module.css'
 
 export function MyCode({ codeblock }: { codeblock: RawCode }) {
-  console.log(codeblock);
-  const codeTitle = codeblock.meta.replace(/title=('|")([^>]+?)('|")/g, '$2');
+  // meta에서 title과 showLineNumbers를 선택적으로 추출
+  const titleMatch = codeblock.meta?.match(/title=('|")([^>]+?)('|")/);
+  const showLineNumbersMatch = codeblock.meta?.match(/showLineNumbers/);
+  
+  // title 값을 추출하거나 null로 설정
+  const codeTitle = titleMatch ? titleMatch[2] : null;
+  // showLineNumbers가 있으면 true, 없으면 false로 설정
+  const showLineNumbers = !!showLineNumbersMatch;
+
+  // handlers 배열 생성, showLineNumbers가 true일 경우에만 lineNumbers 추가
+  const handlers = [callout, mark, diff, className];
+  if (showLineNumbers) {
+    handlers.push(lineNumbers); // showLineNumbers가 true일 때만 추가
+  }
   
   return (
     <div className={clsx('theme-code-block', `language-${codeblock.lang}`, styles.codeBlockContainer)}>
       <CopyButton text={codeblock.code} />
-      {codeblock.meta ? (
+      {codeTitle ? (
         <div className={styles.codeBlockTitle}>{codeTitle}</div>
       ) : null}
       <Pre
         className={styles.codeBlock}
         code={codeblock}
-        handlers={[callout]}
+        handlers={handlers}
         style={{ background: "#212121", color: "#fafafa" }}
       />
     </div>
   )
 }
-
-// title='CommonJS'
-// <div class="language-javascript codeBlockContainer_Ckt0 theme-code-block" style="--prism-color: #bfc7d5; --prism-background-color: #292d3e;"><div class="codeBlockTitle_Ktv7">CommonJS</div><div class="codeBlockContent_biex"><pre tabindex="0" class="prism-code language-javascript codeBlock_bY9V thin-scrollbar" style="color: rgb(191, 199, 213); background-color: rgb(41, 45, 62);"><code class="codeBlockLines_e6Vv"><span class="token-line" style="color: rgb(191, 199, 213);"><span class="token keyword" style="font-style: italic;">var</span><span class="token plain"> whatap </span><span class="token operator" style="color: rgb(137, 221, 255);">=</span><span class="token plain"> </span><span class="token function" style="color: rgb(130, 170, 255);">require</span><span class="token punctuation" style="color: rgb(199, 146, 234);">(</span><span class="token string" style="color: rgb(195, 232, 141);">'whatap'</span><span class="token punctuation" style="color: rgb(199, 146, 234);">)</span><span class="token punctuation" style="color: rgb(199, 146, 234);">.</span><span class="token plain">NodeAgent</span><span class="token punctuation" style="color: rgb(199, 146, 234);">;</span><br></span></code></pre><div class="buttonGroup__atx"><button type="button" aria-label="클립보드에 코드 복사" title="복사" class="clean-btn"><span class="copyButtonIcons_eSgA" aria-hidden="true"><svg viewBox="0 0 24 24" class="copyButtonIcon_y97N"><path fill="currentColor" d="M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z"></path></svg><svg viewBox="0 0 24 24" class="copyButtonSuccessIcon_LjdS"><path fill="currentColor" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"></path></svg></span></button></div></div></div>
---
+++ src/components/codes/line-numbers.tsx
@@ -0,0 +1,21 @@
+import { AnnotationHandler, InnerLine } from "codehike/code"
+import styles from './styles.module.css'
+import clsx from 'clsx'
+
+export const lineNumbers: AnnotationHandler = {
+  name: "line-numbers",
+  Line: (props) => {
+    const width = props.totalLines.toString().length + 1
+    return (
+      <div className={styles.flex}>
+        <span
+          className={clsx(styles.textright, styles.opacity50, styles.selectnone)}
+          style={{ minWidth: `${width}ch` }}
+        >
+          {props.lineNumber}
+        </span>
+        <InnerLine merge={props} className={clsx(styles.flex_1, styles.pl_2)} />
+      </div>
+    )
+  },
+}
