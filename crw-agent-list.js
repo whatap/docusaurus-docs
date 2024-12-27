fe6f887e78ece2eb7da56c60d2aee574fd7d1fd1
ysjo <ysjo@whatap.io>
2024-12-27 09:08:37 +0900

    service-2232 24q4 에이전트 애플리케이션4 python(service-2235)

--- crw-agent-list.js
+++ crw-agent-list.js
@@ -8,7 +8,8 @@ const cheerio = require('cheerio');
 
 // URL 배열 정의
 const urls = [
-    'https://docs.whatap.io/release-notes/nodejs/nodejs-0_5_4'
+    'https://docs.whatap.io/release-notes/python/python-1_7_2',
+    'https://docs.whatap.io/release-notes/python/python-1_7_3',
     // 'https://docs.whatap.io/release-notes/db/dbx-1_8_0',
     // 'https://docs.whatap.io/release-notes/db/dbx-1_8_1',
     // 'https://docs.whatap.io/release-notes/db/dbx-1_9_0',
