1c82bba2ca2533444ab0e98d316e2ca670ffc72f
ysjo <ysjo@whatap.io>
2024-12-27 09:20:32 +0900

    service-2232 24q4 에이전트 서버1(service-2240)

--- crw-agent-list.js
+++ crw-agent-list.js
@@ -8,9 +8,12 @@ const cheerio = require('cheerio');
 
 // URL 배열 정의
 const urls = [
-    'https://docs.whatap.io/release-notes/dotnet/dotnet-2_3_4',
-    'https://docs.whatap.io/release-notes/dotnet/dotnet-2_3_5',
-    'https://docs.whatap.io/release-notes/dotnet/dotnet-2_3_6',
+    'https://docs.whatap.io/release-notes/server/server-2_6_8',
+    'https://docs.whatap.io/release-notes/server/server-2_6_9',
+    'https://docs.whatap.io/release-notes/server/server-2_7_0',
+    'https://docs.whatap.io/release-notes/server/server-2_7_1',
+    'https://docs.whatap.io/release-notes/server/server-2_7_2',
+    'https://docs.whatap.io/release-notes/server/server-2_7_3',
     // 'https://docs.whatap.io/release-notes/db/dbx-1_8_0',
     // 'https://docs.whatap.io/release-notes/db/dbx-1_8_1',
     // 'https://docs.whatap.io/release-notes/db/dbx-1_9_0',
